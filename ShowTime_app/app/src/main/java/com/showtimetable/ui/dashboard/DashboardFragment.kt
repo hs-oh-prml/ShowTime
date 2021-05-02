db89d7883d3a34ed607d7a0aeaadf83fcf3fcffa
Inae Kim <54172475+kia3415@users.noreply.github.com>
2021-05-02 18:01:22 +0900

    일정 추가 방식 Activity 형태로 변경

--- ShowTime_app/app/src/main/java/com/showtimetable/ui/dashboard/DashboardFragment.kt
+++ ShowTime_app/app/src/main/java/com/showtimetable/ui/dashboard/DashboardFragment.kt
@@ -1,27 +1,20 @@
 package com.showtimetable.ui.dashboard
 
 import android.content.Context.INPUT_METHOD_SERVICE
-import android.graphics.PointF
+import android.content.Intent
 import android.os.Bundle
-import android.os.SystemClock
 import android.text.method.ScrollingMovementMethod
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.*
 import android.view.View.*
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.inputmethod.InputMethodManager
-import android.widget.GridLayout
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.showtimetable.CustomToast
 import com.showtimetable.R
-import com.showtimetable.data.CalendarData
 import com.showtimetable.sharedpreference.PreferenceManager
 import kotlinx.android.synthetic.main.fragment_dashboard.*
 import java.util.*
@@ -35,22 +28,15 @@ class DashboardFragment : Fragment() {
     var d = 0
 
     var isSelected = false
-    lateinit var imm:InputMethodManager
-    lateinit var ChangeContent:String
+    lateinit var imm: InputMethodManager
 
-    val week = listOf("일", "월", "화", "수", "목", "금", "토")
-
-    var touch_old=0f
-    var touch_new=0f
-    var disp_width=0
-    var disp_height=0
-    lateinit var mCenter:PointF
-    var  mRadius:Float = 0f
-    var mMaxDist:Float = 0f
-
-//    var mLastClickTime = 0L
-
+    var disp_width = 0
+    var disp_height = 0
     lateinit var adapter: CalendarAdapter
+
+    val WRITE_ACTIVITY_CODE = 101
+    val CANCEL_ACTIVITY_CODE = 100
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -58,16 +44,13 @@ class DashboardFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         init()
     }
 
-    fun init(){
-
-
-        //calendar_frame.layoutParams = RelativeLayout.LayoutParams(MATCH_PARENT,1000)
-
+    fun init() {
         setDisplaySize()
         imm = context!!.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
@@ -77,26 +60,32 @@ class DashboardFragment : Fragment() {
         y = today.get(Calendar.YEAR)
         m = today.get(Calendar.MONTH) + 1
         d = today.get(Calendar.DATE)
-        val listener = object: CalendarAdapter.calendarListener{
-            override fun onClick(month:Int, year:Int, clickDate:Int, weekDay:String, v:View, check:Boolean) {
-//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                if(!check){
-                    val date:String = v.findViewById<TextView>(R.id.date).text.toString()
+        val listener = object : CalendarAdapter.calendarListener {
+            override fun onClick(
+                month: Int,
+                year: Int,
+                clickDate: Int,
+                weekDay: String,
+                v: View,
+                check: Boolean
+            ) {
+                if (!check) {
+                    val date: String = v.findViewById<TextView>(R.id.date).text.toString()
                     y = year
                     m = month
                     d = date.toInt()
                     isSelected = true
 
-                    if(clickDate != -1){
+                    if (clickDate != -1) {
                         val dateStr = "${d}. ${weekDay}"
                         selected_date.text = dateStr
 
                         // c_data: 같은 날의 일정 리스트
                         // CalendarData클래스의 CalendarItem 클래스 리스트
                         // CalendarItem: String Color, String Content
-                        var c_data = pref.getDaySchedule(year, month, date.toInt()).calendarItemList
+                        val c_data = pref.getDaySchedule(year, month, date.toInt()).calendarItemList
 
-                        var listener = object : ContentListAdapter.Listener{
+                        val listener = object : ContentListAdapter.Listener {
                             override fun onClick(pos: Int) {
 
 
@@ -106,8 +95,10 @@ class DashboardFragment : Fragment() {
                                 init()
                             }
                         }
-                        var adapter = ContentListAdapter(requireContext(), c_data, listener, y, m, d)
-                        var layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+                        val adapter =
+                            ContentListAdapter(requireContext(), c_data, listener, y, m, d)
+                        val layoutManager =
+                            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
                         schedule_recycler_view.layoutManager = layoutManager
                         schedule_recycler_view.adapter = adapter
                     }
@@ -116,153 +107,48 @@ class DashboardFragment : Fragment() {
         }
 
         adapter = CalendarAdapter(requireContext(), today, listener, false, y, m, d)
-        //hi
         calendarView.adapter = adapter
         calendarView.setCurrentItem((Integer.MAX_VALUE) / 2, false)
 
-
-        schedule_commit_btn.setOnClickListener {
-            imm.hideSoftInputFromWindow(schedule_edit_text.windowToken, 0)
-            calendar_frame.visibility = VISIBLE
-            schedule_commit_btn.visibility = GONE
-//            schedule_content.visibility = VISIBLE
-            schedule_edit_text.visibility = GONE
-            changeModeBar.visibility = VISIBLE
-            write_schedule.visibility = GONE
-//            selected_date.visibility = VISIBLE
-            schedule_recycler_view.visibility = VISIBLE
-//            add_date_schedule.visibility = VISIBLE
-            selected_bar.visibility = VISIBLE
-
-
-//hi
-            var idx = calendarView.currentItem
-            var adapter = CalendarAdapter(requireContext(), today, listener,false, y, m, d)
-            calendarView.adapter = adapter
-            calendarView.setCurrentItem(idx ,false)
-
-
-            if(schedule_edit_text.text != null && schedule_edit_text.text.toString() != ""){
-                var color = ""
-                var c_str = schedule_edit_text.text.toString()
-                Log.d("Content", c_str)
-                when(color_group.checkedRadioButtonId){
-                    R.id.color1-> color = "#dbc7fb"
-                    R.id.color2-> color = "#c7ecd3"
-                    R.id.color3-> color = "#c3dafc"
-                    R.id.color4-> color = "#f7d1dc"
-                    R.id.color5-> color = "#f8ec9b"
-                    else -> color = "#dbc7fb"
-                }
-
-                pref.addDaySchedule(y, m, d, c_str, color)
-
-                var c_data = pref.getDaySchedule(y, m, d).calendarItemList
-                var list_listener = object : ContentListAdapter.Listener{
-                    override fun onClick(pos: Int) {
-                    }
-
-                    override fun refresh() {
-                        init()
-                    }
-                }
-                var listAdapter = ContentListAdapter(requireContext(), c_data, list_listener, y, m, d)
-                var layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
-                schedule_recycler_view.layoutManager = layoutManager
-                schedule_recycler_view.adapter = listAdapter
-
-                val str = m.toString() + "월 "+ d + "일" + " 일정이 추가되었습니다."
-                CustomToast(requireContext(), str).show()
-                schedule_edit_text.text.clear()
-            }
-        }
-        schedule_content.movementMethod = object: ScrollingMovementMethod(){
+        schedule_content.movementMethod = object : ScrollingMovementMethod() {
         }
 
 
         add_date_schedule.setOnClickListener {
-            if(isSelected){
-                schedule_date.text = selected_date.text.toString()
-                schedule_commit_btn.visibility = VISIBLE
-                selected_bar.visibility = GONE
-                calendar_frame.visibility = GONE
-                schedule_edit_text.visibility = VISIBLE
-                changeModeBar.visibility = GONE
-                write_schedule.visibility = VISIBLE
-                schedule_recycler_view.visibility = GONE
-                var c_data = ArrayList<CalendarData.CalendarItem>()
-                var list_listener = object : ContentListAdapter.Listener{
-                    override fun onClick(pos: Int) {
-
-
-                    }
-
-                    override fun refresh() {
-                        init()
-                    }
-                }
-                var listAdapter = ContentListAdapter(requireContext(), c_data, list_listener, y ,m, d)
-                var layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
-                schedule_recycler_view.layoutManager = layoutManager
-                schedule_recycler_view.adapter = listAdapter
+            if (isSelected) {
+                val intent = Intent(requireContext(), WriteScheduleActivity::class.java)
+                startActivityForResult(intent, WRITE_ACTIVITY_CODE)
             }
-            ChangeContent = schedule_content.text.toString()
         }
 
-        close_write.setOnClickListener {
-            imm.hideSoftInputFromWindow(schedule_edit_text.windowToken, 0)
-            calendar_frame.visibility = VISIBLE
-            schedule_commit_btn.visibility = GONE
-            schedule_edit_text.visibility = GONE
-            changeModeBar.visibility = VISIBLE
-            write_schedule.visibility = GONE
-            schedule_recycler_view.visibility = VISIBLE
-            selected_bar.visibility = VISIBLE
-            var c_data = pref.getDaySchedule(y, m, d).calendarItemList
-            var listener = object : ContentListAdapter.Listener{
-                override fun onClick(pos: Int) {
-//                                    TODO("Not yet implemented")
-//                                schedule_edit_text.setText(c_data[pos].content)
-//                                    schedule_delete_btn.visibility = VISIBLE
 
-                }
-
-                override fun refresh() {
-//                                TODO("Not yet implemented")
-                    init()
-                }
-            }
-            var adapter = ContentListAdapter(requireContext(), c_data, listener, y, m, d)
-            var layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
-            schedule_recycler_view.layoutManager = layoutManager
-            schedule_recycler_view.adapter = adapter
-        }
 
         changeModeBar.setOnClickListener {
-//            if (SystemClock.elapsedRealtime() - mLastClickTime < 100){
-//                return@setOnClickListener;
-//            }
-//            mLastClickTime = SystemClock.elapsedRealtime()
-
-//            listener.onClick(m, y, d)
-
-            if(calendar_bottom.visibility == GONE){
+            if (calendar_bottom.visibility == GONE) {
                 //내용세부화
                 var idx = calendarView.currentItem
-                adapter = CalendarAdapter(requireContext(), today, listener,false, y, m, d)
+                adapter = CalendarAdapter(requireContext(), today, listener, false, y, m, d)
                 calendarView.adapter = adapter
-                calendarView.setCurrentItem(idx ,false)
-                //changeModeBar.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.light_blue))
+                calendarView.setCurrentItem(idx, false)
                 calendar_bottom.visibility = VISIBLE
-                arrow_image.setBackground(ContextCompat.getDrawable(context!!, R.drawable.down_arrow))
-            }
-            else{
+                arrow_image.setBackground(
+                    ContextCompat.getDrawable(
+                        context!!,
+                        R.drawable.down_arrow
+                    )
+                )
+            } else {
                 //동그라미만
                 var idx = calendarView.currentItem
-                adapter = CalendarAdapter(requireContext(), today, listener,true, y, m, d)
+                adapter = CalendarAdapter(requireContext(), today, listener, true, y, m, d)
                 calendarView.adapter = adapter
-                calendarView.setCurrentItem(idx ,false)
-                changeModeBar.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+                calendarView.setCurrentItem(idx, false)
+                changeModeBar.setBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.white
+                    )
+                )
                 calendar_bottom.visibility = GONE
                 arrow_image.setBackground(ContextCompat.getDrawable(context!!, R.drawable.up_arrow))
             }
@@ -271,12 +157,48 @@ class DashboardFragment : Fragment() {
 
     }
 
-    fun setDisplaySize(){
-        var disp = DisplayMetrics()
+    private fun setDisplaySize() {
+        val disp = DisplayMetrics()
         this.activity?.windowManager?.defaultDisplay?.getMetrics(disp)
         disp_width = disp.widthPixels
         disp_height = disp.heightPixels
-        println("disp height "+disp_height)
+        println("disp height " + disp_height)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == WRITE_ACTIVITY_CODE) {
+            if (resultCode == WRITE_ACTIVITY_CODE) {
+                val schedule = data?.getStringExtra("schedule")!!
+                val color = data?.getStringExtra("color")!!
+
+                pref.addDaySchedule(y, m, d, schedule, color)
+
+                val c_data = pref.getDaySchedule(y, m, d).calendarItemList
+                val list_listener = object : ContentListAdapter.Listener {
+                    override fun onClick(pos: Int) {
+                    }
+
+                    override fun refresh() {
+                        init()
+                    }
+                }
+                val listAdapter =
+                    ContentListAdapter(requireContext(), c_data, list_listener, y, m, d)
+                val layoutManager =
+                    LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+                schedule_recycler_view.layoutManager = layoutManager
+                schedule_recycler_view.adapter = listAdapter
+
+                val str = m.toString() + "월 " + d + "일" + " 일정이 추가되었습니다."
+                CustomToast(requireContext(), str).show()
+                schedule_edit_text.text.clear()
+
+            } else if (resultCode == CANCEL_ACTIVITY_CODE) {
+                return
+            }
+        }
+
     }
 
 }
